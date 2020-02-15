4bd67e644c11d544b9252bd6627789a904c7e438
thenullterminator <dazz2803@gmail.com>
2020-02-15 20:00:46 +0530

    Integrated Azure Face

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = 3008 || process.env.PORT;
+const port = 3001 || process.env.PORT;
 const morgan=require('morgan');
 const wallet = require('ethereumjs-wallet');
 // const Web3 = require('web3');
@@ -9,7 +9,7 @@ const app = express();
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(bodyParser({limit: '50mb'}));
 // parse application/json
 app.use(bodyParser.json());
 app.use(morgan('dev'));
@@ -28,6 +28,54 @@ app.post('/createWallet',(req,res)=>{
   });
 
 });
+
+app.post('/send',(req,res)=>{
+  
+  res.send({
+    num:10,
+    A:A,
+    B:B
+  });
+
+});
+
+app.post('/recieve',(req,res)=>{
+
+  var C=req.body;
+  console.log(C);
+});
+
+
+var N=1000;
+var RANGE=1000;
+//create matrix
+var A=new Array(N);
+for(var i=0 ; i<N ; i++)
+{
+  A[i]=new Array(N);
+}
+var B=new Array(N);
+for(var i=0 ; i<N ; i++)
+{
+  B[i]=new Array(N);
+}
+//generate random data
+
+for(var i=0 ; i<N ; i++)
+{
+  for(var j=0; j<N ; j++)
+  {
+    A[i][j]=Math.floor((Math.random()*RANGE) + 1);
+  }
+}
+for(var i=0 ; i<N ; i++)
+{
+  for(var j=0; j<N ; j++)
+  {
+    B[i][j]=Math.floor((Math.random()*RANGE) + 1);
+  }
+}
+
 
 
 app.listen(port, () => {
---
+++ src/computation/task.js
@@ -0,0 +1,57 @@
+
+let xhr = new XMLHttpRequest();
+xhr.open('POST', '../../send');
+xhr.responseType = 'json';
+xhr.send();
+xhr.onload = function() {
+        if (xhr.status != 200) { // analyze HTTP status of the response
+                console.log(" Error in sending");
+        } else { // show the result
+                // alert(`Done, got ${xhr.response.length} bytes`); // responseText is the server
+                console.log("Done!!");
+                map(xhr.response.A,xhr.response.B);
+        }
+};
+
+
+function map(A,B) {
+        var C=new Array(1000);
+        for(var i=0 ; i<1000 ; i++)
+                C[i]=new Array(1000);
+        //generate random data
+        reduce(A,B,C);
+}
+
+function reduce(A,B,C){
+        console.log("In A");
+        console.log(A);
+        console.log(B);
+        console.log("In reduce");
+        for(var i=0;i<1000;i++)
+        {
+                for(var j=0;j<1000;j++)
+                {
+                for(var k=0;k<1000;k++)
+                {
+                        C[i][j]=(A[i][k]*B[k][j])%100;
+                }
+                }
+        }
+        console.log(C);
+
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', '../../recieve');
+        xhr.setRequestHeader("Content-type", "application/json");
+        // xhr.responseType = 'json';
+        console.log(C);
+        xhr.send(JSON.stringify(C));
+        xhr.onload = function() {
+                if (xhr.status != 200) { // analyze HTTP status of the response
+                        console.log(" Error in sending");
+                } else { // show the result
+                        // alert(`Done, got ${xhr.response.length} bytes`); // responseText is the server
+                        console.log("Done!!");
+                        // map(res.A,res.B);
+                }
+        };
+}
